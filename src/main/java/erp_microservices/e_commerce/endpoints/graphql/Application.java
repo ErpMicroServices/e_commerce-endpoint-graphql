3f3176dd118f4511c43c0e180eaf9505a61f4c82
Jim Barrows <Jim.Barrows@gmail.com>
2019-11-01 14:59:27 -0500

    Graphql responds to an introspection commands

--- src/main/java/erp_microservices/e_commerce/endpoints/graphql/Application.java
+++ src/main/java/erp_microservices/e_commerce/endpoints/graphql/Application.java
@@ -1,25 +1,32 @@
 package erp_microservices.e_commerce.endpoints.graphql;
 
-import erp_microservices.e_commerce.endpoints.graphql.resolvers.Mutation;
-import erp_microservices.e_commerce.endpoints.graphql.resolvers.Query;
+import graphql.Scalars;
+import graphql.schema.GraphQLObjectType;
+import graphql.schema.GraphQLSchema;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+
 @SpringBootApplication
 public class Application {
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
-//	@Bean
-//	Mutation mutation() {
-//		return new Mutation();
-//	}
-//
-//	@Bean
-//	Query query() {
-//		return new Query();
-//	}
-	
+	@Bean
+	GraphQLSchema schema() {
+		return GraphQLSchema.newSchema()
+				       .query(GraphQLObjectType.newObject()
+						              .name("query")
+						              .field(field -> field
+								                              .name("test")
+								                              .type(Scalars.GraphQLString)
+								                              .dataFetcher(environment -> "response")
+						              )
+						              .build())
+				       .build();
+	}
 }
